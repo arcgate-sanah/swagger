01bca420287e6a105ac841ca1974b944b5d06141
sanahm23 <sanahmathur@gmail.com>
2023-09-04 10:48:08 +0530

    4/9/23

--- api/views.py
+++ api/views.py
@@ -1,24 +1,21 @@
-from django.shortcuts import render
-from rest_framework.response import Response
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from base.models import Item
 from .serializers import ItemSerializers
-
-
-
-from drf_yasg.utils import swagger_auto_schema
 
 @swagger_auto_schema(method='get', responses={200: ItemSerializers(many=True)})
 @api_view(['GET'])
 def getData(request):
     items = Item.objects.all()
-    serializer = ItemSerializers(items,many=True)
+    serializer = ItemSerializers(items, many=True)
     return Response(serializer.data)
 
-
+@swagger_auto_schema(method='post', request_body=ItemSerializers)
 @api_view(['POST'])
 def postData(request):
     serializer = ItemSerializers(data=request.data)
     if serializer.is_valid():
         serializer.save()
-    return Response(serializer.data)
+        return Response(serializer.data, status=201) 
+    return Response(serializer.errors, status=400)  
--- myproject/settings.py
+++ myproject/settings.py
@@ -1,12 +1,10 @@
 
 
 from pathlib import Path
-from . import environ
+import os
+from decouple import config
+DEBUG=config('DEBUG', cast=bool, default=True)
 
-env = environ.Env(
-    DEBUG=(bool, False),
-)
-environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -15,12 +13,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY=config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+
+ALLOWED_HOSTS = []
 
 
 # Application definition
